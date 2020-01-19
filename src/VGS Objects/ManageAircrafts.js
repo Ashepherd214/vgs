9324dca08f52f0efcc9e7ccf1a986ae05d95d42e
Aaron Shepherd <ashepherd214@gmail.com>
2020-01-19 17:14:38 -0600

    Edit Modal added
    Added in a modal to allow for an edit aircraft form. Form is not currently set to populate from the aircraft data in the table nor is it set to update the database yet.

--- src/VGS Objects/ManageAircrafts.js
+++ src/VGS Objects/ManageAircrafts.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 // import ReactDOM from "react-dom";
 import firebase from "../Firestore";
-import BootstrapTable from 'react-bootstrap-table-next'
+import BootstrapTable from "react-bootstrap-table-next";
+import cellEditFactory, { Type } from "react-bootstrap-table2-editor";
+import EditAircraft from "./EditAircraft";
 import BuildAircraftTable from "./BuildAircraftTable";
 import AddAircraft from "./AddAircraft";
 import {
@@ -12,215 +14,270 @@ import {
   Modal
 } from "react-bootstrap";
 
-
 export class ManageAircrafts extends Component {
-         constructor(props) {
-           super(props);
-           this.state = {
-             aircrafts: [],
-             selected: [],
-             show: false
-           };
+  constructor(props) {
+    super(props);
+    this.state = {
+      aircrafts: [],
+      selected: [],
+      showAdd: false,
+      showEdit: false
+    };
+  }
 
-         }
+  componentDidMount = () => {
+    console.log("Component mounted");
+    const db = firebase.firestore().collection("Aircrafts");
+    this.gotData();
+  };
 
-         componentDidMount = () => {
-           console.log("Component mounted")
-           //const db = firebase.firestore().collection("Aircrafts")
-           this.gotData()
-         };
+  gotData = () => {
+    console.log("Getting Data");
+    let aircrafts = [];
 
-         gotData = () => {
-           console.log("Getting Data")
-           let aircrafts =[]
+    const db = firebase.firestore().collection("Aircrafts");
 
-            const db = firebase.firestore().collection("Aircrafts");
+    db.get()
+      .then(querySnapshot => {
+        //const aircraft = [];
 
-            db.get()
-              .then(querySnapshot => {
-                //const aircraft = [];
+        querySnapshot.forEach(function(doc) {
+          aircrafts.push({
+            name: doc.id,
+            xa: doc.data().Xa,
+            xe: doc.data().Xe,
+            za: doc.data().Za,
+            ze: doc.data().Ze,
+            cg: doc.data().cg,
+            flaps: doc.data().flaps,
+            lookdown: doc.data().lookdown,
+            pitch: doc.data().pitch,
+            speed: doc.data().speed,
+            weight: doc.data().weight,
+            unitsAir: doc.data().unitsAir.toString()
+          });
+        });
 
-                querySnapshot.forEach(function(doc) {
-                  aircrafts.push({
-                    name: doc.id,
-                    xa: doc.data().Xa,
-                    xe: doc.data().Xe,
-                    za: doc.data().Za,
-                    ze: doc.data().Ze,
-                    cg: doc.data().cg,
-                    flaps: doc.data().flaps,
-                    lookdown: doc.data().lookdown,
-                    pitch: doc.data().pitch,
-                    speed: doc.data().speed,
-                    weight: doc.data().weight,
-                    unitsAir: doc.data().unitsAir.toString()
-                  });
-                });
+        this.setState({ aircrafts });
+      })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
+      });
+    console.log(this.state.aircrafts);
+  };
 
-                this.setState({ aircrafts });
-              })
-              .catch(function(error) {
-                console.log("Error getting documents: ", error);
-              });
-              console.log(this.state.aircrafts)
-          }
+  showModal = () => {
+    this.setState({
+      show: !this.state.show
+    });
+    console.log(this.state.show);
+  };
 
-         showModal = () => {
-           this.setState({
-             show: !this.state.show
-           });
-           console.log(this.state.show);
-         };
+  closeModal = () => {
+    this.setState({
+      show: !this.state.show
+    });
+    this.handleUpdate();
+  };
 
-         closeModal = () => {
-           this.setState({
-             show: !this.state.show
-           })
-           this.handleUpdate()
-         }
+  handleOnSelect = (row, isSelect, rowKey) => {
+    if (isSelect) {
+      const craft = this.node.selectionContext.selected;
+      this.setState(() => ({
+        selected: [...this.state.selected, row.id]
+      }));
+    } else {
+      this.setState(() => ({
+        selected: this.state.selected.filter(x => x !== row.id)
+      }));
+    }
+    //alert(db.table.getPageByRowKey(rowKey))
+  };
 
-          handleOnSelect = (row, isSelect, rowKey) => {
-            if (isSelect) {
-              const craft = this.node.selectionContext.selected
-              console.log(craft)
-              this.setState(() => ({
-                selected: [...this.state.selected, row.id]
-              }))
-            } else {
-              this.setState(() => ({
-                selected: this.state.selected.filter(x => x !== row.id)
-              }))
+  // find selected row key and delete that aircraft from database
+  delAircraft = () => {
+    alert(this.node.selectionContext.selected);
+    const selections = [this.node.selectionContext.selected];
+    const db = firebase.firestore().collection("Aircrafts");
+
+    selections.forEach(key => {
+      console.log(
+        db
+          .doc(key.toString())
+          .get()
+          .then(function(doc) {
+            console.log(doc.data());
+          })
+      );
+      db.doc(key.toString())
+        .delete()
+        .then(function() {
+          console.log("Deletion successful!");
+        })
+        .catch(function(error) {
+          console.error("Something went wrong, document not removed");
+        });
+    });
+
+    console.log(this.state.show);
+  };
+
+  editAircraft = () => {
+    // find selected row key and take user input to .set aircraft values for database
+    console.log(this.state.show);
+  };
+
+  render() {
+    let closeAdd = () => this.setState({ showAdd: false });
+    let closeEdit = () => this.setState({ showEdit: false });
+
+    const selectRowProp = {
+      mode: "checkbox",
+      bgColor: "lightblue",
+      clickToEdit: true,
+      clickToSelect: true, //enable click to select
+      hideSelectColumn: true,
+      //selected: this.state.selected,
+      onSelect: this.handleOnSelect
+    };
+
+    const columns = [
+      {
+        dataField: "name",
+        text: "Aircraft Name"
+      },
+      {
+        dataField: "xa",
+        text: "Aircraft Xa Value"
+      },
+      {
+        dataField: "xe",
+        text: "Aircraft Xe Value"
+      },
+      {
+        dataField: "za",
+        text: "Aircraft Za Value"
+      },
+      {
+        dataField: "ze",
+        text: "Aircraft Ze Value"
+      },
+      {
+        dataField: "cg",
+        text: "Aircraft's CG"
+      },
+      {
+        dataField: "flaps",
+        text: "Aircraft Flaps Setting",
+        editor: {
+          type: Type.SELECT,
+          options: [
+            {
+              value: "1",
+              label: "1"
+            },
+            {
+              value: "2",
+              label: "2"
+            },
+            {
+              value: "3",
+              label: "3"
+            },
+            {
+              value: "4",
+              label: "4"
+            },
+            {
+              value: "5",
+              label: "5"
             }
-            console.log(this.node.selectionContext.selected)
-            //alert(db.table.getPageByRowKey(rowKey))
-          }
+          ]
+        }
+      },
+      {
+        dataField: "lookdown",
+        text: "Aircraft Lookdown Value"
+      },
+      {
+        dataField: "pitch",
+        text: "Aircraft Pitch"
+      },
+      {
+        dataField: "speed",
+        text: "Aircraft Speed"
+      },
+      {
+        dataField: "weight",
+        text: "Aircraft Weight"
+      },
+      {
+        dataField: "unitAir",
+        text: "Metric?"
+      }
+    ];
 
-          // find selected row key and delete that aircraft from database
-         delAircraft = () => {
-           alert(this.node.selectionContext.selected)
-            const selections = [this.node.selectionContext.selected]
-           const db = firebase.firestore().collection("Aircrafts")
+    return (
+      <div>
+        <BootstrapTable
+          bootstrap4
+          ref={n => (this.node = n)}
+          id="airTable"
+          keyField="name"
+          data={this.state.aircrafts}
+          columns={columns}
+          selectRow={selectRowProp}
+          cellEdit={cellEditFactory({ mode: "click" })}
+        />
 
-            selections.forEach(key => {
-              console.log(db.doc(key.toString()).get().then(function(doc) { console.log(doc.data())}))
-              db.doc(key.toString()).delete().then(function() {
-                console.log("Deletion successful!")
-              }).catch(function(error) {
-                console.error("Something went wrong, document not removed")
-              })
-            })
+        <ButtonToolbar>
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={() => this.setState({ showAdd: true })}
+          >
+            Add Aircraft
+          </Button>
+          <Button
+            variant="success"
+            size="lg"
+            onClick={() => this.setState({ showEdit: true })}
+          >
+            Edit Aircraft
+          </Button>
+          <Button variant="danger" size="lg" onClick={this.delAircraft}>
+            Delete Aircraft
+          </Button>
+        </ButtonToolbar>
 
-           
-           console.log(this.state.show)
-         };
-
-         editAircraft = () => {
-           // find selected row key and take user input to .set aircraft values for database
-           console.log(this.state.show)
-         };
-
-         render() {
-           let { show } = this.state;
-
-           const selectRowProp = {
-             mode: 'checkbox',
-             bgColor: 'lightblue',
-             clickToSelect: true, //enable click to select 
-             hideSelectColumn: true,
-             //selected: this.state.selected,
-             onSelect: this.handleOnSelect
-           }
-
-           const columns = [
-             {
-               dataField: "name",
-               text: "Aircraft Name"
-             },
-             {
-               dataField: "xa",
-               text: "Aircraft Xa Value"
-             },
-             {
-               dataField: "xe",
-               text: "Aircraft Xe Value"
-             },
-             {
-               dataField: "za",
-               text: "Aircraft Za Value"
-             },
-             {
-               dataField: "ze",
-               text: "Aircraft Ze Value"
-             },
-             {
-               dataField: "cg",
-               text: "Aircraft's CG"
-             },
-             {
-               dataField: "flaps",
-               text: "Aircraft Flaps Setting"
-             },
-             {
-               dataField: "lookdown",
-               text: "Aircraft Lookdown Value"
-             },
-             {
-               dataField: "pitch",
-               text: "Aircraft Pitch"
-             },
-             {
-               dataField: "speed",
-               text: "Aircraft Speed"
-             },
-             {
-               dataField: "weight",
-               text: "Aircraft Weight"
-             },
-             {
-               dataField: "unitAir",
-               text: "Metric?"
-             }
-           ];
-
-           return (
-             <div>
-               <BootstrapTable
-                  bootstrap4
-                  ref={ n => this.node = n }
-                  id="airTable"
-                  keyField='name'
-                  data={ this.state.aircrafts }
-                  columns={ columns }
-                  selectRow={ selectRowProp }
-               />
-                
-               <ButtonToolbar>
-                 <Button variant="primary" size="lg" onClick={this.showModal}>
-                   Add Aircraft
-                 </Button>
-                 <Button variant="success" size="lg" onClick={this.editAircraft}>
-                   Edit Aircraft
-                 </Button>
-                 <Button variant="danger" size="lg" onClick={this.delAircraft}>
-                   Delete Aircraft
-                 </Button>
-               </ButtonToolbar>
-
-               <Modal id="addAirModal" show={show} onHide={this.showModal}>
-                 <Modal.Header closeButton>
-                   <Modal.Title>Aircraft Editor</Modal.Title>
-                 </Modal.Header>
-                 <Modal.Body>
-                   <AddAircraft id="airForm" />
-                 </Modal.Body>
-                 <Modal.Footer>
-                   <Button variant="danger" onClick={this.showModal}>
-                     Close
-                   </Button>
-                 </Modal.Footer>
-               </Modal>
-             </div>
-           );
-         }
-       }
+        <Modal id="addAirModal" show={this.state.showAdd} onHide={closeAdd}>
+          <Modal.Header closeButton>
+            <Modal.Title>Aircraft Editor</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <AddAircraft id="airForm" />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={closeAdd}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal id="editAirModal" show={this.state.showEdit} onHide={closeEdit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Aircraft Editor</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <EditAircraft id="editAirForm" />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={closeEdit}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
 export default ManageAircrafts;
