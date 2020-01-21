9b2777c5791e14b5b624a9ae936ebb4803088e93
Aaron S <ashepherd214@gmail.com>
2020-01-21 14:27:59 -0600

    Edit Function Finished
    Edit aircraft table functionality has been completed. Need to tidy up closing after submission and getting the metric/unit value to show correctly in the table.

--- src/VGS Objects/EditAircraft.js
+++ src/VGS Objects/EditAircraft.js
@@ -6,110 +6,67 @@ import { Formik, Field, ErrorMessage } from "formik";
 import ManageAircrafts from "./ManageAircrafts";
 
 class EditAircraft extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.select = [props.select];
-    
     this.state = {
-      Ze: " ",
-      Xe: " ",
-      Lookdown: " ",
-      Za: " ",
-      Xa: " ",
-      Flaps: " ",
-      Speed: " ",
-      Weight: " ",
-      CG: " ",
-      Pitch: " ",
-      Units: false,
+      name: " ",
+      ze: " ",
+      xe: " ",
+      lookdown: " ",
+      za: " ",
+      xa: " ",
+      flaps: " ",
+      speed: " ",
+      weight: " ",
+      cg: " ",
+      pitch: " ",
+      units: false,
     };
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.loadCurrentValues = this.loadCurrentValues.bind(this)
   }
 
-  componentDidMount() {
-    let aircraft = [];
+  async componentDidMount() {
+    
+    this.loadCurrentValues();
+  }
 
-    this.aircraft = this.aircraft.bind(this);    
-
+  loadCurrentValues = () => {
     const select = this.props.name
     const selString = select[0].toString()
-    console.log("The aircraft being passed is: ", selString)
+    console.log("Current bind: ", this.state.name)
 
     const airRef = firebase.firestore().collection("Aircrafts").doc(selString)
-    // const query = airRef.where("id", "==", { select })
 
-    airRef.get().then(function(doc) {
+    airRef.get()
+    .then(doc => {
       if (doc.exists) {
-        console.log("Document Data Passed: ", doc.data())
-        aircraft.push({
+        console.log("state of values: ", this.state.name)
+        this.setState({
           name: doc.id,
-          xa: doc.data().Xa,
-          xe: doc.data().Xe,
-          za: doc.data().Za,
           ze: doc.data().Ze,
-          cg: doc.data().cg,
-          flaps: doc.data().flaps,
+          xe: doc.data().Xe,
           lookdown: doc.data().lookdown,
-          pitch: doc.data().pitch,
+          za: doc.data().Za,
+          xa: doc.data().Xa,
+          flaps: doc.data().flaps,
           speed: doc.data().speed,
           weight: doc.data().weight,
-          unitsAir: doc.data().unitsAir 
+          cg: doc.data().cg,
+          pitch: doc.data().pitch, 
+          units: doc.data().unitsAir
         })
+        console.log("Document Data Set: ", this.name )
       } else {
         console.log("No such document!")
       }
-    }).catch(function(error) {
+    }).catch(function (error) {
       console.log("Error getting document: ", error)
     })
-    // .then( (querySnapshot) => {
-
-    //   querySnapshot.forEach(function(doc) {
-    //     console.log(doc.data())
-    //   });
-    // })
-    // .catch(function(error) {
-    //   console.log("Error getting documents: ", error)
-    // })
-
-    //console.log(doc.data())
-
-    // db.get()
-    //   .then(function (doc) {
-    //     aircraft.push({
-    //       name: doc.id,
-    //       xa: doc.data().Xa,
-    //       xe: doc.data().Xe,
-    //       za: doc.data().Za,
-    //       ze: doc.data().Ze,
-    //       cg: doc.data().cg,
-    //       flaps: doc.data().flaps,
-    //       lookdown: doc.data().lookdown,
-    //       pitch: doc.data().pitch,
-    //       speed: doc.data().speed,
-    //       weight: doc.data().weight,
-    //       unitsAir: doc.data().unitsAir.toString()
-    //     });
-    //     this.setState({ aircraft });
-    //   })
-    //   .catch(function (error) {
-    //     console.log("Error getting documents: ", error);
-    //   });
-  }
-  /*updateInfo = event => {
-    this.setState({
-      [event.target.acName]: event.target.value
-      Ze: 0.0,
-                Xe: 0.0,
-                Lookdown: 0.0,
-                Za: 0.0,
-                Xa: 0.0,
-                Flaps: 0,
-                Speed: 0,
-                Weight: 0.0,
-                CG: 0.0,
-                Pitch: 0.0,
-                Units: true 
-    });
-  }; */
+  }  
 
   updateAircraft = values => {
     const db = firebase.firestore();
@@ -132,62 +89,10 @@ class EditAircraft extends React.Component {
         pitch: Number(values.pitch),
         unitsAir: Boolean(values.unitsAir)
       });
-    // After submission values are cleared
-    /*this.setState({
-      acName: " ",
-      ze: " ",
-      xe: " ",
-      lookdown: " ",
-      za: " ",
-      xa: " ",
-      flaps: " ",
-      speed: " ",
-      weight: " ",
-      cg: " ",
-      pitch: " ",
-      units: true
-    });*/
   };
 
   render() {
-
-    // let aircraft = [];
-
-    // const select = this.props.name
-    // console.log("passed data: ", select)
-    // const db = firebase.firestore().collection("Aircrafts").doc(select[0].toString())
-
-    // db.get()
-    //   .then(function (doc) {
-    //       aircraft.push({
-    //         name: doc.id,
-    //         xa: doc.data().Xa,
-    //         xe: doc.data().Xe,
-    //         za: doc.data().Za,
-    //         ze: doc.data().Ze,
-    //         cg: doc.data().cg,
-    //         flaps: doc.data().flaps,
-    //         lookdown: doc.data().lookdown,
-    //         pitch: doc.data().pitch,
-    //         speed: doc.data().speed,
-    //         weight: doc.data().weight,
-    //         unitsAir: doc.data().unitsAir.toString()
-    //       });
-    //     //this.setState({ aircraft });
-    //     })
-    //   .catch(function (error) {
-    //     console.log("Error getting documents: ", error);
-    //   });
-
-    //   console.log(aircraft)
-    // db.get()
-    //   .then(function(doc) {
-    //     console.log("Document found: ", doc.data())
-    //   })
-
     return (
-      
-
       <div className="container">
         <div className="row mb-5">
           <div className="col-lg-12 text-center">
@@ -195,22 +100,22 @@ class EditAircraft extends React.Component {
           </div>
         </div>
         <div className="row">
-          <div className="col-lg-12">         
+          <div className="col-lg-12">       
             <Formik
             enableReinitialize = {true}
               initialValues={{
-                acName: this.aircraft.name,
-                ze: this.aircraft.ze,
-                xe: " ",
-                lookdown: " ",
-                za: " ",
-                xa: " ",
-                flaps: " ",
-                speed: " ",
-                weight: " ",
-                cg: " ",
-                pitch: " ",
-                unitsAir: true
+                acName: this.state.name,
+                ze: this.state.ze,
+                xe: this.state.xe,
+                lookdown: this.state.lookdown,
+                za: this.state.za,
+                xa: this.state.xa,
+                flaps: this.state.flaps,
+                speed: this.state.speed,
+                weight: this.state.weight,
+                cg: this.state.cg,
+                pitch: this.state.pitch,
+                unitsAir: this.state.units
               }}
               validate={values => {
                 const errors = {};
@@ -238,7 +143,7 @@ class EditAircraft extends React.Component {
                 values
               }) => (
                 <Form onSubmit={handleSubmit}>
-                  <Form.Group controlId="formName">
+                  <Form.Group controlId="editForm">
                     <Form.Label>Aircraft Name</Form.Label>
                     <Form.Control
                       value={values.acName}
@@ -252,115 +157,145 @@ class EditAircraft extends React.Component {
                     />
                   </Form.Group>
                   <br />
-                  <Field
-                    value={values.ze}
-                    id="ze"
-                    name="ze"
-                    type="number"
-                    //placeholder="Enter Ze value"
-                    className={`form-control ${
-                      touched.ze && errors.ze ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Ze</Form.Label>
+                    <Field
+                      value={values.ze}
+                      id="ze"
+                      name="ze"
+                      type="number"
+                      //placeholder="Enter Ze value"
+                      className={`form-control ${
+                        touched.ze && errors.ze ? "is-invalid" : ""
+                      }`}
+                    />
+                    </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="xe"
-                    name="xe"
-                    type="number"
-                    //placeholder="Enter Xe value"
-                    className={`form-control ${
-                      touched.xe && errors.xe ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Xe</Form.Label>
+                    <Field
+                      value={values.xe}
+                      id="xe"
+                      name="xe"
+                      type="number"
+                      //placeholder="Enter Xe value"
+                      className={`form-control ${
+                        touched.xe && errors.xe ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="lookdown"
-                    name="lookdown"
-                    type="number"
-                    //placeholder="Enter lookdown value"
-                    className={`form-control ${
-                      touched.lookdown && errors.Lookdown ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Lookdown Value</Form.Label>
+                    <Field
+                      value={values.lookdown}
+                      id="lookdown"
+                      name="lookdown"
+                      type="number"
+                      //placeholder="Enter lookdown value"
+                      className={`form-control ${
+                        touched.lookdown && errors.Lookdown ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="za"
-                    name="za"
-                    type="number"
-                    //placeholder="Enter Za value"
-                    className={`form-control ${
-                      touched.za && errors.za ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Za</Form.Label>
+                    <Field
+                      value={values.za}
+                      id="za"
+                      name="za"
+                      type="number"
+                      //placeholder="Enter Za value"
+                      className={`form-control ${
+                        touched.za && errors.za ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="xa"
-                    name="xa"
-                    type="number"
-                    //placeholder="Enter Xa value"
-                    className={`form-control ${
-                      touched.xa && errors.xa ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Xa</Form.Label>
+                    <Field
+                      value={values.xa}
+                      id="xa"
+                      name="xa"
+                      type="number"
+                      //placeholder="Enter Xa value"
+                      className={`form-control ${
+                        touched.xa && errors.xa ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="flaps"
-                    name="flaps"
-                    type="number"
-                    //placeholder="Enter Flaps setting"
-                    className={`form-control ${
-                      touched.flaps && errors.flaps ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Flaps</Form.Label>
+                    <Field
+                      value={values.flaps}
+                      id="flaps"
+                      name="flaps"
+                      type="number"
+                      //placeholder="Enter Flaps setting"
+                      className={`form-control ${
+                        touched.flaps && errors.flaps ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="speed"
-                    name="speed"
-                    type="number"
-                    //placeholder="Enter Aircraft's speed"
-                    className={`form-control ${
-                      touched.speed && errors.speed ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Speed</Form.Label>
+                    <Field
+                      value={values.speed}
+                      id="speed"
+                      name="speed"
+                      type="number"
+                      //placeholder="Enter Aircraft's speed"
+                      className={`form-control ${
+                        touched.speed && errors.speed ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="weight"
-                    name="weight"
-                    type="number"
-                    //placeholder="Enter Aircraft's weight"
-                    className={`form-control ${
-                      touched.weight && errors.weight ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Weight</Form.Label>
+                    <Field
+                      value={values.weight}
+                      id="weight"
+                      name="weight"
+                      type="number"
+                      //placeholder="Enter Aircraft's weight"
+                      className={`form-control ${
+                        touched.weight && errors.weight ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="cg"
-                    name="cg"
-                    type="number"
-                    //placeholder="Enter Aircraft's CG"
-                    className={`form-control ${
-                      touched.cg && errors.cg ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft CG</Form.Label>
+                    <Field
+                      value={values.cg}
+                      id="cg"
+                      name="cg"
+                      type="number"
+                      //placeholder="Enter Aircraft's CG"
+                      className={`form-control ${
+                        touched.cg && errors.cg ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
-                  <Field
-                    value={values.name}
-                    id="pitch"
-                    name="pitch"
-                    type="number"
-                    //placeholder="Enter Aircraft's pitch angle"
-                    className={`form-control ${
-                      touched.pitch && errors.pitch ? "is-invalid" : ""
-                    }`}
-                  />
+                  <Form.Group controlId="editForm">
+                    <Form.Label>Aircraft Pitch</Form.Label>
+                    <Field
+                      value={values.pitch}
+                      id="pitch"
+                      name="pitch"
+                      type="number"
+                      //placeholder="Enter Aircraft's pitch angle"
+                      className={`form-control ${
+                        touched.pitch && errors.pitch ? "is-invalid" : ""
+                      }`}
+                    />
+                  </Form.Group>
                   <br />
                   <label htmlFor="unitsAir">
                     Unit of measurement for values
@@ -371,7 +306,7 @@ class EditAircraft extends React.Component {
                     id="unitsAir"
                     name="unitsAir"
                     type="select"
-                    value={values.name}
+                    value={values.units}
                   >
                     <option value="true">Metric</option>
                     <option value="false">Imperial</option>
